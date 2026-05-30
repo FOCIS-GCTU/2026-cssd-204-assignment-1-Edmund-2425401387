44296c9fc6e422eca153e20097faadfb85dd9fd8
Edmund-2425401387 <2425401387@live.gctu.edu.gh>
2026-05-30 22:49:44 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Prints my initials in block letter format.
+# Assignment Number: 1
+#
+# Name: Edmund Okai Aryee.
+# Student ID: 2425401387.
+# Email: 2425401387@live.gctu.edu.gh (Student)
+# Grader: Augustus Buckman.
+#
+# On my honor, Edmund Aryee, this programming assignment is my
+# own work and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This program prints my initials in a block letter format
+    print()
+    print("...EOA")
+    print()
+    print("...EEEEEEEEEEEE............OOOO................AAAA.........") #1
+    print("...EEEEEEEEEEEE..........OOOOOOOO............AAAAAAAA.......") #2
+    print("...EEEEE...............OOO......OOO.........AAAA...AAAA.....") #3
+    print("...EEEEE...............OOO......OOO.........AAAA...AAAA.....") #4
+    print("...EEEEEEEEEEEE........OOO......OOO.........AAAA...AAAA.....") #5
+    print("...EEEEEEEEEEEE........OOO......OOO.........AAAAAAAAAAA.....") #6
+    print("...EEEEE...............OOO......OOO.........AAAAAAAAAAA.....") #7
+    print("...EEEEE...............OOO......OOO.........AAAA...AAAA.....") #8
+    print("...EEEEEEEEEEEE...**.....OOOOOOOO.....**....AAAA...AAAA...**") #9
+    print("...EEEEEEEEEEEE...**.......OOOO.......**....AAAA...AAAA...**") #10
+    print()
 
 
 main()
